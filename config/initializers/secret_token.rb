1676a10942c1d591b2beb8a73b1e345d8c6ff47c
kubicue <3x2media@gmail.com>
2013-06-30 15:56:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovevqs::Application.config.secret_token = '8259643e5ad514ca0339d99d9346a871412d0ba29c7f357c7d3ff79ca862f899a7fc117f3d38bae203d3a60235a6ba76b19e419a94e731a3692641a2a183715a'
